92e53fe3c055ff00d361f00eee10a6fda437c533
Sam Nixon <samnixon87@gmail.com>
2021-02-15 10:06:32 +0000

    Fix the default value for the book changer

--- src/Books.js
+++ src/Books.js
@@ -16,7 +16,7 @@ class Books extends Component {
                     <div className="book-cover" style={{ width:128, height: 193, backgroundImage: `url(${book.imageLinks &&
                 book.imageLinks.thumbnail})`}}></div>
                     <div className="book-shelf-changer">
-                        <select defaultValue={book.shelf} onChange ={(event) => onChangeShelf(event, book)}>
+                        <select defaultValue={book.shelf ? book.shelf : 'none'} onChange ={(event) => onChangeShelf(event, book)}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
